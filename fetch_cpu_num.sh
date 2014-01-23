14da593473195b717ddd400c664da7bf71e96af8
yongfengxia <yongfengxia@tencent.com>
2014-01-23 10:56:24 +0800

    fix bug

--- fetch_cpu_num.sh
+++ fetch_cpu_num.sh
@@ -2,7 +2,7 @@
 
 while read line
 do
-    cpu_num=$(ssh -q -n root@$line 'grep cpu /proc/stat | wc -l')-1
+    cpu_num=$(ssh -q -n root@$line 'grep cpu /proc/stat | wc -l')
     let cpu_num=cpu_num-1
     echo $line" "$cpu_num >> server_with_cpunum.txt
 done < server_list.txt
